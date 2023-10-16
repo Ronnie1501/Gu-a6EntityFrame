76d14556714d1d00e872ef67604d928ddddb427a
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-10-15 22:48:40 -0600

    Initial commit

---
+++ Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guía6EntityFrame
+{
+    public class Context : DbContext
+    {
+        public DbSet<Student> estudiante { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6O6146B;Database=PrograDB; Trusted_Connection = SSPI; MultipleActiveResultSets = true; Trust Server Certificate = true; ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Guía6EntityFrame;
+
+using (var contextdb = new Context())
+{
+    foreach (var item in contextdb.estudiante)
+    {
+        Console.WriteLine("Datos:  Id: " + item.Id + " Nombre: " + item.Nombre + " Apellidos: " + item.Apellidos + " Edad: " + item.Edad + " Sexo: " + item.Sexo);
+
+    }
+}
+
+bool AgregarRegistros = true;
+
+while (AgregarRegistros)
+{
+    Console.WriteLine("Ingrese los datos del estudiante:");
+
+    Console.Write("Nombre: ");
+    string nombre = Console.ReadLine();
+
+    Console.Write("Apellidos: ");
+    string apellidos = Console.ReadLine();
+
+    Console.Write("Sexo: ");
+    string sexo = Console.ReadLine();
+
+    Console.Write("Edad: ");
+    int edad = Convert.ToInt32(Console.ReadLine());
+
+    using (var contextdb = new Context())
+    {
+        contextdb.Database.EnsureCreated();
+
+        var nuevoEstudiante = new Student()
+        {
+            Nombre = nombre,
+            Apellidos = apellidos,
+            Sexo = sexo,
+            Edad = edad
+        };
+
+        contextdb.Add(nuevoEstudiante);
+        contextdb.SaveChanges();
+    }
+
+    Console.WriteLine("¿Agregar más registros? (S/N)");
+    string respuesta = Console.ReadLine();
+    AgregarRegistros = respuesta.StartsWith("S", StringComparison.OrdinalIgnoreCase);
+}
+
+using (var contextdb = new Context())
+{
+    foreach (var estudiante in contextdb.estudiante)
+    {
+        Console.WriteLine("Estudiante: " + estudiante.Nombre + estudiante.Apellidos + " Sexo: " + estudiante.Sexo + " Edad: " + estudiante.Edad);
+    }
+}
